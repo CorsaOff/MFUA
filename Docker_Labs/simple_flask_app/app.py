36f584c1763932af856e00f48bec0475643e6ec1
CorsaOff <danasavalskij28224@gmail.com>
2026-05-06 09:57:10 +0300

    Update

---
+++ Docker_Labs/simple_flask_app/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, Docker! Привет из Flask!"
+
+@app.route('/health')
+def health():
+    return {"status": "healthy"}, 200
+
+if __name__ == '__main__':
+    # Слушаем 0.0.0.0, чтобы Docker мог пробросить трафик внутрь
+    app.run(host='0.0.0.0', port=5000, debug=True)
